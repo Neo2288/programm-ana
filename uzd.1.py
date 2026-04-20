0e83dea0eb7dbe08f30f6aa7f4023a631371052d
Neo2288 <kristapskusnirs6@gmail.com>
2026-04-20 14:51:35 +0300

    Add files via upload

--- uzd.1.py
+++ uzd.1.py
@@ -1,12 +1,33 @@
-import csv
-datne=open("kontakti.csv",encoding="utf-8")
-kontakti=list(csv.reader(datne))
-datne.close()
+import json
+with open("skoleni_atzimes.json",encoding="utf-8") as datne:
+    dati=json.load(datne)
+    vardi=[]
+    for i in dati:
+       #print(i["name"])
+       vardi.append(i["name"])
+vardi.sort()
+print(vardi)
+for i in dati:
+    # print(i["name"])
+    #vardi.append(i["name"])
+    #print(i["grades"]["Fizika"])
+    if i["grades"]["Fizika"] < 4:
+        print(i["name"])
+for i in dati:
+    vid=(i["grades"]["Matematika"]+i["grades"]["Fizika"]+i["grades"]["Informatika"])/3
+    print(i["name"],":",vid)
+for i in dati:
+    i["grades"].update({"Sports":8})
+    print(i["grades"])
+mat=[]
+a=0
+for i in dati:
+    mat.append(i["grades"]["Matematika"])
+print("min",min(mat))
+print("max",max(mat))
+print("10:",mat.count(10))
 
-galvene=["vārds", "uzvārds", "telefons", "pilsēta"]
-for i in kontakti:
-    print(i[0],i[-2])
-with open("k1.csv","w",newline="", encoding="utf-8") as fails:
-    a=csv.writer(fails, delimiter="\t")
-    a.writerow(galvene)
-    a.writerows(kontakti)
+v=input("skolēna vārds:")
+for i in dati:
+    i.get(v,"Skolēna nav")
+    print()
