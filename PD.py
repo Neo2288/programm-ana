389b0db9bbca9db7b84be16702eaee92c6dbc655
Neo2288 <kristapskusnirs6@gmail.com>
2026-04-21 09:34:52 +0300

    Add files via upload

--- PD.py
+++ PD.py
@@ -1,10 +1,13 @@
-#
-for i in range(4,10):
-    v=input("ievadi vārdu:")
-    if len(v)<4:
-        continue
-    else:
-        print(f"Virknē {v.capitalize()} ir {len(v)} simbolu")
-
-
+import json
+import csv
+with open("uzd1.json",encoding="utf-8") as datne:
+    dati=json.load(datne)
+#print(type(dati))
+valstis=[]
+lauksaimniecibas_ipatsvars=[]
+for i in dati:
+    valstis.append(i["nosaukums"])
+    lauksaimniecibas_ipatsvars.append((i["lauksaimniecibas_zeme"]/(i["lauksaimniecibas_zeme"]+i["mezs"]+i["cita_veida_zeme"]))*100)
+print(valstis)
+print(lauksaimniecibas_ipatsvars)
 
